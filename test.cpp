95122ea06027f855b4bdb2ef7acacb34abb7fd71
Duy Nguyen <nguyenhaduy@compute-linux1.cs.tamu.edu>
2016-10-04 07:55:37 -0500

    Some of unit test

--- test.cpp
+++ test.cpp
@@ -1,7 +1,7 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 #include "DB_Engine.h"
 #include "Tuple.h"
 #include "Table.h"
@@ -13,8 +13,9 @@ Attribute name;
 Attribute mascot;
 Attribute conquests;
 vector<Attribute> templ_attr;
-Tuple templ;
+Tuple templ, tuple0, tuple1, tuple2;
 Table* relation;
+Table* relation2;
 
 void make_relation(){
 	//Make relation
@@ -24,6 +25,7 @@ void make_relation(){
 	templ_attr.push_back(name);
 	templ_attr.push_back(mascot);
 	templ_attr.push_back(conquests);
+	templ= Tuple(templ_attr);
 	relation = new Table("Pirates", templ);
  
 	name.set_String_Value("Bob");
@@ -34,9 +36,9 @@ void make_relation(){
 	templ_attr[1] = mascot;
 	templ_attr[2] = conquests;
   
-	templ = Tuple(templ_attr);
+	tuple0 = Tuple(templ_attr);
 
-	relation->insert(templ);
+	relation->insert(tuple0);
   
 	name.set_String_Value("Joe");
 	mascot.set_String_Value("skull");
@@ -46,9 +48,9 @@ void make_relation(){
 	templ_attr[1] = mascot;
 	templ_attr[2] = conquests;
 
-	templ = Tuple(templ_attr);
+	tuple1 = Tuple(templ_attr);
 
-	relation->insert(templ);
+	relation->insert(tuple1);
 
 	name.set_String_Value("Davy");
 	mascot.set_String_Value("octopus");
@@ -58,23 +60,152 @@ void make_relation(){
 	templ_attr[1] = mascot;
 	templ_attr[2] = conquests;
 
-	templ = Tuple(templ_attr);
+	tuple2 = Tuple(templ_attr);
 
-	relation->insert(templ);
+	relation->insert(tuple2);
 }
 
 
 TEST_CASE( "Test Making Relation")
 {
-	make_relation();
+	make_relation();	
+	vector<Tuple> check_tuple;
+	check_tuple = relation->get_Tuples();
 	REQUIRE (relation->get_Name() == "Pirates");
-	REQUIRE (relation->get_Template_Tuple().get_Attribute == 3);
+	REQUIRE (relation->get_Template_Tuple() == templ);
+	REQUIRE (check_tuple.size() == 3);
+	REQUIRE (check_tuple[0] == tuple0);
+	REQUIRE (check_tuple[1] == tuple1);
+	REQUIRE (check_tuple[2] == tuple2);
 }
 
-TEST_CASE( "Test Creating Table")
+TEST_CASE( "Test Creating Relation")
 {
-	make_relation();
-	Table* relation2 = DB_Engine::create_relation("Pirates2",templ);
-	DB_Engine::show(*relation2);
+	relation2 = DB_Engine::create_relation("Pirates2",templ);
+	vector<Tuple> check_tuple;
+	check_tuple = relation2->get_Tuples();
 	REQUIRE (relation2->get_Name() == "Pirates2");
+	REQUIRE (relation2->get_Template_Tuple() == templ);
+	REQUIRE (check_tuple.size() == 0);
 }
+
+TEST_CASE( "Test Insert Relation")
+{
+	DB_Engine::insert_relation(relation2, *relation);	
+	vector<Tuple> check_tuple;
+	check_tuple = relation2->get_Tuples();
+	REQUIRE (relation2->get_Name() == "Pirates2");
+	REQUIRE (relation2->get_Template_Tuple() == templ);
+	REQUIRE (check_tuple.size() == 3);
+	REQUIRE (check_tuple[0] == tuple0);
+	REQUIRE (check_tuple[1] == tuple1);
+	REQUIRE (check_tuple[2] == tuple2);
+}
+
+TEST_CASE( "Test Select With Small Number of Conquests Condition")
+{
+	// selecting pirates with conquests less than 30 from relation2
+	Condition_Sing few_conquest_sing("conquests", "<=", "30");
+	Condition few_conquest(few_conquest_sing);
+	Table* query = Relation_Ops::select(few_conquest, *relation2);	
+	vector<Tuple> check_tuple;
+	check_tuple = query->get_Tuples();
+	REQUIRE (query->get_Name() == "select_table");
+	REQUIRE (query->get_Template_Tuple() == templ);
+	REQUIRE (check_tuple.size() == 2);
+	REQUIRE (check_tuple[0] == tuple0);
+	REQUIRE (check_tuple[1] == tuple1);
+}
+
+TEST_CASE( "Test Remove Tuples Based on Conditions")
+{
+	// removing pirates with conquests less than 30 from relation2
+	Condition_Sing few_conquest_sing("conquests", "<=", "30");
+	Condition few_conquest(few_conquest_sing);
+	DB_Engine::remove_query(relation2, few_conquest);
+	vector<Tuple> check_tuple;
+	check_tuple = relation2->get_Tuples();
+	REQUIRE (relation2->get_Name() == "Pirates2");
+	REQUIRE (relation2->get_Template_Tuple() == templ);
+	REQUIRE (check_tuple.size() == 1);
+	REQUIRE (check_tuple[0] == tuple2);
+}
+
+TEST_CASE( "Test Project From Relation1")
+{
+	// removing pirates with conquests less than 30 from relation2
+	vector<string> attributes;
+	attributes.push_back("name");
+	attributes.push_back("mascot");
+	Table* query = Relation_Ops::project(attributes, *relation);
+
+	vector<Tuple> check_tuple;
+
+	Attribute name, mascot;
+	vector<Attribute> templ_attr;
+
+	name = Attribute("", true, "name", 10);
+	mascot = Attribute("", false, "mascot", 10);
+	templ_attr.push_back(name);
+	templ_attr.push_back(mascot);
+	Tuple templ_check(templ_attr);
+
+	name.set_String_Value("Bob");
+	mascot.set_String_Value("parrot");
+	templ_attr[0] = name;
+	templ_attr[1] = mascot;  
+	Tuple tuple_check0 (templ_attr);
+  
+	name.set_String_Value("Joe");
+	mascot.set_String_Value("skull");
+	templ_attr[0] = name;
+	templ_attr[1] = mascot;
+	Tuple tuple_check1 (templ_attr);
+
+	name.set_String_Value("Davy");
+	mascot.set_String_Value("octopus");
+	templ_attr[0] = name;
+	templ_attr[1] = mascot;
+	Tuple tuple_check2 (templ_attr);
+
+	check_tuple = query->get_Tuples();
+	REQUIRE (query->get_Name() == "project_table");
+	// REQUIRE (query->get_Template_Tuple() == templ_check);
+	REQUIRE (check_tuple.size() == 3);
+	REQUIRE (check_tuple[0] == tuple_check0);
+	REQUIRE (check_tuple[1] == tuple_check1);
+	REQUIRE (check_tuple[2] == tuple_check2);
+}
+
+TEST_CASE( "Test Project From Relation2")
+{
+	// removing pirates with conquests less than 30 from relation2
+	vector<string> attributes;
+	attributes.push_back("name");
+	attributes.push_back("mascot");
+	Table* query = Relation_Ops::project(attributes, *relation2);
+
+	vector<Tuple> check_tuple;
+
+	Attribute name, mascot;
+	vector<Attribute> templ_attr;
+
+	name = Attribute("", true, "name", 10);
+	mascot = Attribute("", false, "mascot", 10);
+	templ_attr.push_back(name);
+	templ_attr.push_back(mascot);
+	Tuple templ_check(templ_attr);
+
+	name.set_String_Value("Davy");
+	mascot.set_String_Value("octopus");
+	templ_attr[0] = name;
+	templ_attr[1] = mascot;
+	Tuple tuple_check (templ_attr);
+
+	check_tuple = query->get_Tuples();
+	REQUIRE (query->get_Name() == "project_table");
+	// REQUIRE (query->get_Template_Tuple() == templ_check);
+	REQUIRE (check_tuple.size() == 1);
+	REQUIRE (check_tuple[0] == tuple_check);
+}
+
